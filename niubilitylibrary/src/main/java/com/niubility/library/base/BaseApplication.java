0e8f141dfb70647e9dc51ac8f04019f37f8d4ffb
liaoweitao <992020815@qq.com>
2019-01-15 18:34:54 +0800

    基础库- 添加错误信息写入日志功能，捕获全局异常

--- niubilitylibrary/src/main/java/com/niubility/library/base/BaseApplication.java
+++ niubilitylibrary/src/main/java/com/niubility/library/base/BaseApplication.java
@@ -2,18 +2,96 @@ package com.niubility.library.base;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.os.Environment;
+
+import com.niubility.library.tools.CrashHandler;
+import com.niubility.library.tools.FileUtils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class BaseApplication extends Application {
 
     public static Context sApplication;
+    protected boolean openCrashHandler = false;
+    public File logDir;
+
     @Override
     public void onCreate() {
         super.onCreate();
         sApplication = this;
 
+        if(isOpenCrashHandler()) {
+
+            initDir();
+
+            CrashHandler crashHandler = CrashHandler.getInstance();
+            crashHandler.init(sApplication, new CrashHandler.OnCrashHandleListener() {
+                @Override
+                public void onCrashHandle(Thread t, Throwable e) {
+                    onCrash(t, e);
+                }
+            });
+        }
+
     }
+
+    protected boolean isOpenCrashHandler() {
+        return openCrashHandler;
+    }
+
+
+    private void initDir() {
+
+        String internalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        logDir = new File(internalStoragePath
+                + File.separator + "zaojiu"
+                + File.separator + "log");
+
+        if (!logDir.exists()){
+            logDir.mkdirs();
+        }
+    }
+
+    public void onCrash(Thread t, Throwable e) {
+        writeCrashLog(e);
+    }
+
+
+    private void writeCrashLog(Throwable e) {
+        StringBuilder sb = new StringBuilder();
+        String exceptionString = FileUtils.getExceptionString(e);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+        String formatDate = simpleDateFormat.format(new Date());
+
+        String content = sb.append("====================crash====================").append("\n")
+                .append(formatDate).append("\n")
+                .append(exceptionString)
+                .append("\n")
+                .append("\n")
+                .toString();
+
+        File testFile = new File(logDir, "crash.txt");
+        FileUtils.writeToFile(testFile, content, true);
+    }
+
+//    private void restartAppDelayed(long delayMillis) {
+//        Intent intent = new Intent(sApplication, MainActivity.class);
+//        @SuppressLint("WrongConstant") PendingIntent restartIntent = PendingIntent.getActivity(
+//                mContext, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+//        //退出程序
+//        AlarmManager mgr = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+//        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + delayMillis,
+//                restartIntent); // 5秒钟后重启应用
+//
+//        //结束进程之前可以把你程序的注销或者退出代码放在这段代码之前
+//        android.os.Process.killProcess(android.os.Process.myPid());
+//    }
 
 
 }
---
+++ niubilitylibrary/src/main/java/com/niubility/library/tools/CrashHandler.java
@@ -0,0 +1,80 @@
+package com.niubility.library.tools;
+
+import android.content.Context;
+
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    public static final String TAG = CrashHandler.class.getSimpleName();
+    private static CrashHandler INSTANCE = new CrashHandler();
+    private Context mContext;
+    private Thread.UncaughtExceptionHandler mDefaultHandler;
+
+    private OnCrashHandleListener mListener;
+
+    public interface OnCrashHandleListener {
+        void onCrashHandle(Thread t, Throwable e);
+    }
+
+
+    private CrashHandler() {
+    }
+
+
+    public static CrashHandler getInstance() {
+        return INSTANCE;
+    }
+
+
+    public void init(Context ctx, OnCrashHandleListener listener) {
+        mContext = ctx;
+        mListener = listener;
+        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+
+        e.printStackTrace();
+        if(mListener != null) {
+            mListener.onCrashHandle(t, e);
+        }
+//        writeCrashLog(e);
+//        restartAppDelayed(2000);
+    }
+//
+//    private void writeCrashLog(Throwable e) {
+//        StringBuilder sb = new StringBuilder();
+//        String exceptionString = FileUtils.getExceptionString(e);
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+//        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+//        String formatDate = simpleDateFormat.format(new Date());
+//
+//        String content = sb.append("====================crash====================").append("\n")
+//                .append(formatDate).append("\n")
+//                .append("订单号：" + Config.out_trade_no).append("\n")
+//                .append(exceptionString)
+//                .append("\n")
+//                .append("\n")
+//                .toString();
+//
+//        File testFile = new File(MyApplication.logDir, "crash.txt");
+//        FileUtils.writeToFile(testFile, content, true);
+//    }
+//
+//    private void restartAppDelayed(long delayMillis) {
+//        Intent intent = new Intent(mContext, MainActivity.class);
+//        @SuppressLint("WrongConstant") PendingIntent restartIntent = PendingIntent.getActivity(
+//                mContext, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+//        //退出程序
+//        AlarmManager mgr = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+//        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + delayMillis,
+//                restartIntent); // 5秒钟后重启应用
+//
+//        //结束进程之前可以把你程序的注销或者退出代码放在这段代码之前
+//        android.os.Process.killProcess(android.os.Process.myPid());
+//    }
+
+
+
+}
