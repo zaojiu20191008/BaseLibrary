8274f38d85df1366eadccd2e9ae9945783e0e5f3
liaoweitao <992020815@qq.com>
2019-04-30 18:53:09 +0800

    基础库 添加阴影扩散效果drawable，添加隐藏导航栏的flag

--- niubilitylibrary/src/main/java/com/niubility/library/utils/ScreenUtils.java
+++ niubilitylibrary/src/main/java/com/niubility/library/utils/ScreenUtils.java
@@ -85,7 +85,7 @@ public class ScreenUtils {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
 //        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 //                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
